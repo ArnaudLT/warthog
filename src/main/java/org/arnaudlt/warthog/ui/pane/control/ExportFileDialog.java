b60585b5a6d97a6f73cb60085f867feaa3adac50
Arnaud <arn.letort@gmail.com>
2021-05-09 14:43:41 +0200

    Rework export UI

--- src/main/java/org/arnaudlt/warthog/ui/pane/control/ExportFileDialog.java
+++ src/main/java/org/arnaudlt/warthog/ui/pane/control/ExportFileDialog.java
@@ -6,6 +6,7 @@ import javafx.geometry.Orientation;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
@@ -55,9 +56,9 @@ public class ExportFileDialog {
         this.owner = owner;
         this.dialog = StageFactory.buildModalStage(owner, "Export to file");
 
-        GridPane grid = GridFactory.buildGrid();
-
-        int i = 0;
+        // Basic settings
+        GridPane basicGrid = GridFactory.buildGrid();
+        int rowIndex = 0;
 
         Label outputLabel = new Label("Output directory :");
         TextField output = new TextField();
@@ -72,10 +73,10 @@ public class ExportFileDialog {
             output.setText(exportFile.getAbsolutePath());
         });
 
-        grid.add(outputLabel, 0, i);
-        grid.add(output, 1, i, 3, 1);
-        grid.add(outputButton, 4, i);
-        i++;
+        basicGrid.add(outputLabel, 0, rowIndex);
+        basicGrid.add(output, 1, rowIndex, 3, 1);
+        basicGrid.add(outputButton, 4, rowIndex);
+        rowIndex++;
 
         Label formatLabel = new Label("Format :");
         ComboBox<Format> format = new ComboBox<>(FXCollections.observableArrayList(Format.values()));
@@ -86,22 +87,13 @@ public class ExportFileDialog {
         ComboBox<String> saveMode = new ComboBox<>(FXCollections.observableArrayList("Overwrite", "Append"));
         saveMode.setValue("Overwrite");
         saveMode.setMinWidth(100);
-
-        grid.addRow(i++, formatLabel, format, modeLabel, saveMode);
-
-        Label partitionByLabel = new Label("Partitions :");
-        TextField partitionBy = new TextField();
-        partitionBy.setTooltip(new Tooltip("Comma separated list of attributs"));
-
-        grid.add(partitionByLabel, 0, i);
-        grid.add(partitionBy, 1, i, 3, 1);
-        i++;
+        basicGrid.addRow(rowIndex++, formatLabel, format, modeLabel, saveMode);
 
         // Start conditional display
         BooleanBinding csvSelected = format.valueProperty().isEqualTo(Format.CSV);
         Separator s = new Separator(Orientation.HORIZONTAL);
         s.visibleProperty().bind(csvSelected);
-        grid.add(s, 0, i++, 4, 1);
+        basicGrid.add(s, 0, rowIndex++, 4, 1);
 
         Label separatorLabel = new Label("Separator :");
         separatorLabel.visibleProperty().bind(csvSelected);
@@ -114,11 +106,29 @@ public class ExportFileDialog {
         CheckBox header = new CheckBox();
         header.setSelected(true);
         header.visibleProperty().bind(csvSelected);
-        grid.addRow(i++, separatorLabel, separator, headerLabel, header);
+        basicGrid.addRow(rowIndex++, separatorLabel, separator, headerLabel, header);
         // End conditional display
 
-        grid.add(new Separator(Orientation.HORIZONTAL), 0, i++, 4, 1);
+        Tab basicSettingsTab = new Tab("Settings", basicGrid);
 
+        // Advanced settings
+        GridPane advancedGrid = GridFactory.buildGrid();
+        rowIndex = 0;
+
+        Label partitionByLabel = new Label("Partitions :");
+        TextField partitionBy = new TextField();
+        partitionBy.setMinWidth(300);
+        partitionBy.setMaxWidth(300);
+        partitionBy.setTooltip(new Tooltip("Comma separated list of attributs"));
+
+        advancedGrid.addRow(rowIndex++, partitionByLabel, partitionBy);
+
+        Tab advancedSettingsTab = new Tab("Advanced", advancedGrid);
+
+        TabPane tabPane = new TabPane(basicSettingsTab, advancedSettingsTab);
+        tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
+
+        GridPane bottomGrid = GridFactory.buildGrid();
         Button exportButton = new Button("Export");
         exportButton.setOnAction(event -> {
 
@@ -127,9 +137,9 @@ public class ExportFileDialog {
             exportToFile(exportFileSettings);
             dialog.close();
         });
-        grid.addRow(i, exportButton);
+        bottomGrid.addRow(0, exportButton);
 
-        Scene dialogScene = StageFactory.buildScene(grid);
+        Scene dialogScene = StageFactory.buildScene(new VBox(tabPane, bottomGrid));
         dialog.setScene(dialogScene);
     }
 
