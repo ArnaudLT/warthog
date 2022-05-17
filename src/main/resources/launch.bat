6cf8099251f1a073235a771ee30a92cf900416be
ArnaudLT <arn.letort@gmail.com>
2022-05-17 18:08:12 +0200

    Packaging seems to work :-)

--- src/main/resources/launch.bat
+++
@@ -1,2 +0,0 @@
-set HADOOP_HOME=%cd%/winutils/
-java -jar warthog-1.0-SNAPSHOT.jar
---
+++ src/main/scripts/launch.bat
@@ -0,0 +1,4 @@
+set HADOOP_HOME=%cd%/winutils/
+set JAVA_HOME=%cd%/jre/
+set PATH=%JAVA_HOME%/bin/;%PATH%
+javaw -jar warthog-1.0-SNAPSHOT.jar
