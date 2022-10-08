d6d355b2746ec8a6de5c6b4f34b1e26e282f6021
ArnaudLT <arn.letort@gmail.com>
2022-10-08 18:26:59 +0200

    Remove focus effect on background tasks and history

--- src/main/java/org/arnaudlt/warthog/ui/util/ButtonFactory.java
+++ src/main/java/org/arnaudlt/warthog/ui/util/ButtonFactory.java
@@ -3,12 +3,9 @@ package org.arnaudlt.warthog.ui.util;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.Tooltip;
-import javafx.scene.text.Font;
 
 public class ButtonFactory {
 
-
-    private static final Font SEGOE_MDL2_FONT = new Font("Segoe MDL2 Assets", 12);
 
 
     private ButtonFactory() {}
@@ -17,7 +14,7 @@ public class ButtonFactory {
     public static Button buildSegoeButton(String text, String tooltipText) {
 
         Button button = new Button(text);
-        button.setFont(SEGOE_MDL2_FONT);
+        button.setStyle("-fx-font-family: 'Segoe MDL2 Assets'");
         button.setTooltip(new Tooltip(tooltipText));
         return button;
     }
--- src/main/java/org/arnaudlt/warthog/ui/util/LabelFactory.java
+++ src/main/java/org/arnaudlt/warthog/ui/util/LabelFactory.java
@@ -6,16 +6,13 @@ import javafx.scene.text.Font;
 public class LabelFactory {
 
 
-    private static final Font SEGOE_MDL2_FONT = new Font("Segoe MDL2 Assets", 12);
-
-
     private LabelFactory() {}
 
 
     public static Label buildSegoeLabel(String text) {
 
         Label label = new Label(text);
-        label.setFont(SEGOE_MDL2_FONT);
+        label.setStyle("-fx-font-family: 'Segoe MDL2 Assets'");
         return label;
     }
 
